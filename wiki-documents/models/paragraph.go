c785a9f965f49e17f73b2302482cbc0d526f5f03
ubiquitousbyte <nasdenkov@gmail.com>
2022-01-01 13:51:03 +0100

    remove old models

--- wiki-documents/models/paragraph.go
+++
@@ -1,8 +0,0 @@
-// Package models contains application-specific entities
-package models
-
-type Paragraph struct {
-	Title    string `bson:"title" json:"title"`
-	Position int    `bson:"position" json:"position"`
-	Text     string `bson:"text" json:"text"`
-}
