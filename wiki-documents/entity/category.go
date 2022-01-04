cfe5e66c8b0b294e4b85b15d30509531567be003
ubiquitousbyte <nasdenkov@gmail.com>
2022-01-04 17:31:03 +0100

    omit empty id fields

--- wiki-documents/entity/category.go
+++ wiki-documents/entity/category.go
@@ -1,7 +1,7 @@
 package entity
 
 type Category struct {
-	Id          Id     `bson:"_id,omitempty" json:"id"`
+	Id          Id     `bson:"_id,omitempty" json:"id,omitempty"`
 	Source      string `bson:"source" json:"source"`
 	Name        string `bson:"name" json:"name"`
 	Description string `bson:"description" json:"description"`
