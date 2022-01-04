439b902cc42b6369ae3f482820b12749a3216982
ubiquitousbyte <nasdenkov@gmail.com>
2022-01-04 16:05:38 +0100

    remove redundant handlers

--- wiki-documents/api/handlers/category_handler.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/ubiquitousbyte/wiki-documents/database"
-
-type CategoryHandler struct {
-	storage database.CategoryStore
-}
--- wiki-documents/api/handlers/document_handler.go
+++
@@ -1,9 +0,0 @@
-// Package handlers implements HTTP request handlers
-// for application-specific entities.
-package handlers
-
-import "github.com/ubiquitousbyte/wiki-documents/database"
-
-type DocumentHandler struct {
-	storage *database.DocumentStore
-}
