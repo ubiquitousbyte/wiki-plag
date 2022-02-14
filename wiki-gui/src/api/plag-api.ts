9fddecbc8d2e49cdb366a88152bb661b02465cb0
Atanas Denkov <nasdenkov@gmail.com>
2022-02-14 19:22:54 +0100

    simulate network load time

--- wiki-gui/src/api/plag-api.ts
+++ wiki-gui/src/api/plag-api.ts
@@ -13,27 +13,28 @@ const client = axios.create({
 
 const PlagAPI = {
     detect: async function (text: string): Promise<Paragraph[]> {
-        /* return client.post<Paragraph[]>('/plag', { text: text })
-             .then((response) => response.data)
-             .catch((error) => Promise.reject(APIError.fromResponse(error)));*/
-        return Promise.resolve([
-            new Paragraph("1", "1", "This is some text", 3),
-            new Paragraph("2", "1", "This is some text", 3),
-            new Paragraph("3", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
-            new Paragraph("4", "1", "This is some text", 3),
-            new Paragraph("5", "1", "This is some text", 3),
-            new Paragraph("6", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
-            new Paragraph("7", "1", "This is some text", 3),
-            new Paragraph("8", "1", "This is some text", 3),
-            new Paragraph("9", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
-            new Paragraph("10", "1", "This is some text", 3),
-            new Paragraph("11", "1", "This is some text", 3),
+        return new Promise(r => setTimeout(r, 1000))
+            .then((r) => [
+                new Paragraph("12", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
+                new Paragraph("13", "1", "This is some text", 3),
+                new Paragraph("14", "1", "This is some text", 3),
+                new Paragraph("15", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd  This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
+                new Paragraph("16", "1", "This is some text", 3),
+                new Paragraph("18", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd  This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
+
+            ])
+        /*  return client.post<Paragraph[]>('/plag', { text: text })
+              .then((response) => response.data)
+              .catch((error) => Promise.reject(APIError.fromResponse(error)));*/
+        /*return Promise.resolve([
             new Paragraph("12", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
             new Paragraph("13", "1", "This is some text", 3),
             new Paragraph("14", "1", "This is some text", 3),
             new Paragraph("15", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd  This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
             new Paragraph("16", "1", "This is some text", 3),
-        ])
+            new Paragraph("18", "1", "This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd  This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd This is some more text blah blah blah blah asdas as as da dmagnaigna a s asd ", 3),
+
+        ])*/
     }
 }
 
